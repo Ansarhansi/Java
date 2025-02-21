f8a3b6017571bf4683ac49fd1082c0e15ff1ae21
Ansar Hansi <ansarhansi29@gmail.com>
2025-02-21 10:46:31 +0530

    pushing java Demo

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+	public static void main(String args[]){
+	float stipend=12.000f;
+	string s="xworkz"
+	system.out.println("you got placed in xworkz with stipend :"+stipend);
+	}
+}
