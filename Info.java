0152b46d4dddc9848b5734e5abbf746543101143
Ansarhansi <161307300+Ansarhansi@users.noreply.github.com>
2025-03-27 10:03:35 +0530

    Delete Info.java

--- Info.java
+++
@@ -1,9 +0,0 @@
-public class Info{
-	public static void main(String args[]){
-	 System.out.println("My name is  Ansar");
-	 System.out.println("My Age is 22");
-	 System.out.println("Iam from Hubbali");
-	 System.out.println("Iam form CSE");
-
-	}
-}
