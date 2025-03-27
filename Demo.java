e49712344f27092d120ab9e497a6f9ca24ed7bf8
Ansarhansi <161307300+Ansarhansi@users.noreply.github.com>
2025-03-27 10:02:07 +0530

    Delete Demo.java

--- Demo.java
+++
@@ -1,7 +0,0 @@
-public class Demo{
-	public static void main(String args[]){
-	float stipend=12.000f;
-	string s="xworkz"
-	system.out.println("you got placed in xworkz with stipend :"+stipend);
-	}
-}
